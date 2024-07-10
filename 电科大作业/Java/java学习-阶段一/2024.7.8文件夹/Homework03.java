9d5701e3bd16c2aac286383b70b44313fb2157e4
LOSER <118034542+LoserCoderLi@users.noreply.github.com>
2024-07-10 18:11:58 +0800

    Add files via upload

---
+++ 电科大作业/Java/java学习-阶段一/2024.7.8文件夹/Homework03.java
@@ -0,0 +1,18 @@
+public class Homework03{
+
+	public static void main(String args[]){
+		String book1 = "天龙八部";
+		String book2 = "笑傲江湖";
+		System.out.println(book1 + book2);
+
+		char sex1 = '男';
+		char sex2 = '女';
+		System.out.println(sex1 + sex2);// 男码值 + 女码值
+
+		float price1 = 123.56f;
+		float price2 = 100.56f;
+		System.out.println(price1 + price2);
+
+
+	}
+}
---
+++ 电科大作业/Java/java学习-阶段一/2024.7.8文件夹/Homework04.java
@@ -0,0 +1,14 @@
+public class Homework04{
+
+	public static void main(String args[]){
+
+		String name = "小强";
+		int age = 20;
+		double score = 89.1;
+		char gender = '男';
+		String hobby = "打篮球";
+
+		System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n"+ name+ "\t" + age + "\t" 
+			+ score + "\t" + gender + "\t" + hobby);
+	}
+}
